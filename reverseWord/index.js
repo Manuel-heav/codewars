9e7d36c0ef4eff7522bc7cee357bbcade2206b47
manuel-heav <manuelheav32@gmail.com>
2022-05-09 18:05:01 +0300

    reverse word

---
+++ reverseWord/index.js
@@ -0,0 +1,8 @@
+let str = "Learning is the base of life, said John."
+let s = str.split(" ")
+let ans =""; 
+
+    for (var i = s.length-1; i >= 0; i--) { 
+        ans += s[i] + " "; 
+    } 
+console.log(ans.slice(0,ans.length-1)+".");
