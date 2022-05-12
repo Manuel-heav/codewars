a81288c90d0be68328e386f52f1fbf178caa7747
manuel-heav <manuelheav32@gmail.com>
2022-05-12 20:27:48 +0300

    new

--- reverseWord/index.js
+++ reverseWord/index.js
@@ -3,6 +3,6 @@ let s = str.split(" ")
 let ans =""; 
 
     for (var i = s.length-1; i >= 0; i--) { 
-        ans += s[i] + " "
+        ans += s[i] + " ";
     } 
 console.log(ans.slice(0,ans.length-1)+".");
